e630e99d0dd6cdf94bde3d1643f41d25fdc60ec1
clairezed <clairezed@users.noreply.github.com>
2019-11-22 14:04:37 +0000

    Automated deployment: Fri Nov 22 14:04:37 UTC 2019 b56b76688c46796c2398306f822f9541762f9222

---
+++ js/bundle.js
@@ -0,0 +1,49 @@
+console.log("js ok");
+;
+// INITIALISATION ==================================================
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  // IMG LOADING ---------------------------------------------------
+
+  (() => {
+    'use strict';
+    // Page is loaded
+    const objects = document.getElementsByClassName('asyncImage'); Array.from(objects).map((item) => {
+      // Start loading image
+      const img = new Image();
+      img.src = item.dataset.src;
+      // Once image is loaded replace the src of the HTML element
+      img.onload = () => {
+        item.classList.remove('asyncImage');
+        return item.nodeName === 'IMG' ?
+          item.src = item.dataset.src :
+          item.style.backgroundImage = `url(${item.dataset.src})`;
+      };
+    });
+  })();
+
+  // NAVBAR ==========================================================
+  // Get all "navbar-burger" elements
+  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach(function ($el) {
+      $el.addEventListener('click', function () {
+
+        // Get the target from the "data-target" attribute
+        var target = $el.dataset.target;
+        var $target = document.getElementById(target);
+
+        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+        $el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
+});
